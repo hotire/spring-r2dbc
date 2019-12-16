5693fcc5a548ddd661128f01512d592c0ff1f197
user <gngh0101@gmail.com>
2019-12-16 19:39:07 +0900

    Verify findAll

---
+++ src/test/java/com/github/hotire/springr2dbc/CustomerRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.github.hotire.springr2dbc;
+
+import java.util.Arrays;
+import javax.annotation.PostConstruct;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+
+@DataR2dbcTest
+class CustomerRepositoryTest {
+
+  @Autowired
+  private CustomerRepository repository;
+
+  @PostConstruct
+  void config() {
+    final Flux<Customer> customerFlux = repository.saveAll(Arrays.asList(new Customer("Ho", "Tire"), new Customer("Joo", "Man")));
+    StepVerifier.create(customerFlux).expectNextCount(2).verifyComplete();
+  }
+
+  @Test
+  void findAll() {
+    final Flux<Customer> customerFlux = repository.findAll();
+    StepVerifier.create(customerFlux).expectNextCount(2).verifyComplete();
+  }
+
+}
