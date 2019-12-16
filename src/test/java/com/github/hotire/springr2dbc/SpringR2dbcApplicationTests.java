083e61384b43a14e76444be384dec71e6d48addb
user <gngh0101@gmail.com>
2019-12-16 19:28:49 +0900

    Remove unused imports

--- src/test/java/com/github/hotire/springr2dbc/SpringR2dbcApplicationTests.java
+++ src/test/java/com/github/hotire/springr2dbc/SpringR2dbcApplicationTests.java
@@ -1,10 +1,8 @@
 package com.github.hotire.springr2dbc;
 
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Slf4j
 @SpringBootTest
 class SpringR2dbcApplicationTests {
 
